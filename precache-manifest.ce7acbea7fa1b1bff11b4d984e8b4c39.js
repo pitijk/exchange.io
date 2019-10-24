41dc079fd77feeae4ecd5d853ea4c41b188a8eba
Piotr Kielek <piotrek4kontak@gmail.com>
2019-10-24 10:14:31 +0200

    Updates

---
+++ precache-manifest.ce7acbea7fa1b1bff11b4d984e8b4c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f59db2d681d5fca33f95dd3bdcbbb8a",
+    "url": "/multi-currency-wallet/index.html"
+  },
+  {
+    "revision": "1f6cc2a0e1bb201daec8",
+    "url": "/multi-currency-wallet/static/css/main.f9e4db52.chunk.css"
+  },
+  {
+    "revision": "65d1bbcea7c802c154d4",
+    "url": "/multi-currency-wallet/static/js/2.5a7adad4.chunk.js"
+  },
+  {
+    "revision": "1f6cc2a0e1bb201daec8",
+    "url": "/multi-currency-wallet/static/js/main.950a51f6.chunk.js"
+  },
+  {
+    "revision": "7000b6bd9969f446466f",
+    "url": "/multi-currency-wallet/static/js/runtime-main.31c24174.js"
+  },
+  {
+    "revision": "74f71ff31a38412c85240ef7cf595a82",
+    "url": "/multi-currency-wallet/static/media/back.74f71ff3.png"
+  }
+]);
